7b4bf91865c75d0f6be0641484deb0864df6c835
Weston Sankey <me@westonsankey.com>
2024-09-01 15:10:19 -0400

    add z-index

--- src/App.tsx
+++ src/App.tsx
@@ -48,14 +48,21 @@ function App() {
 }
 
 function Card({ index }: { index: number }) {
-  const { attributes, listeners, setNodeRef, transform, transition } =
-    useSortable({
-      id: index,
-    });
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: index,
+  });
 
   const style = {
     transform: CSS.Transform.toString(transform),
     transition,
+    zIndex: isDragging ? 1 : "auto",
   };
 
   return (
